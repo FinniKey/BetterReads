e4427fe2e4a0c1d3aa0f0d0f8762c9b8312c8b4e
Sam Dunnachie <samdunnachie4@gmail.com>
2024-07-04 21:51:42 +1200

    Complete book review web scrape

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using HtmlAgilityPack;
+﻿using System.Runtime.Versioning;
+using HtmlAgilityPack;
 
 namespace SimpleWebScraper
 {
@@ -27,14 +28,18 @@ namespace SimpleWebScraper
             Console.WriteLine("Hello, World!");
 
             var web = new HtmlWeb();
+            Console.WriteLine("loading webpage");
             var document = web.Load("https://www.goodreads.com/review/list/91520258-jack-edwards?shelf=read"); // loads the webpage
+            Console.WriteLine("webpage loaded");
 
             var bookReviews = new List<BookReview>(); // list of all the book reviews from above
 
+            Console.WriteLine("getting all book reviews");
             var bookReviewHTMLElements = document.DocumentNode.QuerySelectorAll("tr.review");
 
             Console.WriteLine(bookReviewHTMLElements.Count);
 
+            Console.WriteLine("going through reviews");
             foreach (var bookReviewHTMLElement in bookReviewHTMLElements)
             {
                 // if the book actually has a rating then we add it. if it does not, we do not.
@@ -43,13 +48,42 @@ namespace SimpleWebScraper
                 if (bookReviewHTMLElement.QuerySelector("td.rating div span").ChildAttributes("title").Any())
                 {
                     var title = HtmlEntity.DeEntitize(bookReviewHTMLElement.QuerySelector("td.title div a").InnerText);
+                    var titleFormatted = System.Text.RegularExpressions.Regex.Replace(title, @"\s+", "");
                     var url = "https://www.goodreads.com" + HtmlEntity.DeEntitize(bookReviewHTMLElement.QuerySelector("td.title div a").Attributes["href"].Value);
 
                     var ratingTitle = HtmlEntity.DeEntitize(bookReviewHTMLElement.QuerySelector("td.rating div span").Attributes["title"].Value);
 
+                    int rating = 0;
+                    switch (ratingTitle)
+                    {
+                        case "did not like it":
+                            rating = 1;
+                            break;
 
-                    Console.WriteLine(title + ratingTitle + " - " + url);
+                        case "it was ok":
+                            rating = 2;
+                            break;
+
+                        case "liked it":
+                            rating = 3;
+                            break;
+
+                        case "really liked it":
+                            rating = 4;
+                            break;
+
+                        case "it was amazing":
+                            rating = 5;
+                            break;
+                    }
+                    bookReviews.Add(new BookReview() { Url = url, Title = titleFormatted, Rating = rating });
+                    //Console.WriteLine(title);
                 }
+            }
+
+            foreach (var BookReview in bookReviews)
+            {
+                Console.WriteLine(BookReview.Rating + "/5 - " + BookReview.Title + "\n" + BookReview.Url + "\n");
             }
         }
     }
